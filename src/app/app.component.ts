389231bf3ed629a279a9824b38dbb6ee11f341a7
saintur_b <saintur.b@novelsoft.mn>
2022-01-25 11:52:34 +0800

    new detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cvapp';
+  theme = 'new'; // ['google', 'new']
+  user = {
+    firstname: 'Saintor',
+    lastname: 'Batkhuu',
+    fullname: 'Saintor. B',
+    username: 'esaintor',
+    email: 'esaintor@gmail.com',
+    gitlab: 'https://github.com/esaintor',
+    phone: '99162922',
+    position: 'Software Developer',
+    detail: 'Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Proficient in an assortment of technologies, including Java, Angular, Kafka, and Micro-services architecture.'
+  };
   json = {
     title: 'Experiences',
     experiences: [
       {
-        as: 'Senior Developer',
-        where: 'IT Zone LLC',
-        from: 'Aug/2018',
+        as: 'Cheif Technology Architect',
+        where: 'Novelsoft LLC',
+        from: 'Dec/2019',
         to: 'Present',
         items: [
           {
-            description: 'Developed Car Parking System for Shangrila Mall by using Dahua technology SDK which can provide plate number recognition',
-            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot', 'Dahua Technology']
-          },
-          {
-            description: 'Developed Document Management System that would increase productivity in any office by using Tesseract open source to read context in image and pdf files',
-            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot', 'Tesseract']
-          },
+            description: 'Managed multiple development team in the company\n' +
+              'Mainly focused on web application development using Micro-services architecture\n' +
+              'Developed Real-Time Chat applications which can carry a vast number of user and provide video conference all across the platform',
+            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot', 'RabbitMQ', 'Kafka', 'Ream-Time']
+          }
         ]
       },
       {
-        as: 'Junior Developer',
+        as: 'Senior Full-Stack Developer',
         where: 'IT Zone LLC',
-        from: 'Jan/2017',
-        to: 'Aug/2018',
+        from: 'Oct/2018',
+        to: 'Dec/2019',
         items: [
           {
-            description: 'Designed flexible, dynamic form architecture which can adopt in any situation using high level programming language',
-            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot']
-          },
-          {
-            description: 'Created dashboard of custom Kaizen System by using AmChart, D3.js in short period of time',
-            tags: ['Java', 'Javascript', 'C#', 'AmChart', 'D3.js']
+            description: 'Managed a product development team which is responsible for developing Document Management system and Car Parking system. ',
+            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot', 'Dahua Technology', 'Tesseract']
           }
         ]
       },
       {
-        as: 'Student',
-        where: 'Mongolian University of Science and Technology',
-        from: 'Jan/2013',
-        to: 'Jan/2017',
+        as: 'Front-end Developer',
+        where: 'IT Zone LLC',
+        from: 'Feb/2017',
+        to: 'Oct/2018',
         items: [
           {
-            description: 'Created web app using statistical spacio-temporal data to show historical view of temporature diffirences',
-            tags: ['Java', 'Javascript', 'Mapbox', 'Leaflet', 'Spring Boot']
-          },
-          {
-            description: 'Created android app with facial recognition which comes in handy for logging students’attencance for our university',
-            tags: ['Java', 'Android', 'Open CV']
+            description: 'Participated front-end development in the company\'s Location Tracking system, Form Survey Web application, and Content Management system.',
+            tags: ['Java', 'Javascript', 'Angular', 'Spring Boot', 'C#', 'AmChart', 'D3.js']
           }
         ]
-      }
+      },
+      // {
+      //   as: 'Student',
+      //   where: 'Mongolian University of Science and Technology',
+      //   from: 'Jan/2013',
+      //   to: 'Jan/2017',
+      //   items: [
+      //     {
+      //       description: 'Created web app using statistical spacio-temporal data to show historical view of temporature diffirences',
+      //       tags: ['Java', 'Javascript', 'Mapbox', 'Leaflet', 'Spring Boot']
+      //     },
+      //     {
+      //       description: 'Created android app with facial recognition which comes in handy for logging students’attencance for our university',
+      //       tags: ['Java', 'Android', 'Open CV']
+      //     }
+      //   ]
+      // }
     ],
     certificate: 'Certificates',
     certificates: [
@@ -90,6 +108,18 @@ export class AppComponent {
         to: 'Jul/2013',
         tags: ['Java', 'Javascript', 'Microsoft office', 'Web', 'Windows 7']
       }
+    ],
+    skills: [
+      'java',
+      'spring framework',
+      'typescript',
+      'microservices',
+      'kafka',
+      'rabbit mq',
+      'react',
+      'native android',
+      'native swift',
+      'docker'
     ]
   };
 }
